2a280d0b88b6550fdb960ccb05429febfd20ce04
Nikolay <reviakin223@gmail.com>
2022-11-27 22:07:55 +0300

    completion

--- app/Footer/Footer.tsx
+++ app/Footer/Footer.tsx
@@ -1,21 +1,29 @@
 import { FC } from "react";
+import { useMatchMedia } from "../hooks/useMatchMedia";
 
 import ContactsFooter from "./ContactsFooter/ContactsFooter";
 import style from "./footer.module.scss";
 import Note from "./Note/Note";
 import Section from "./Section/Section";
-import { sectionData } from "./Section/section.data";
+import { sectionData, sectionDataMobile } from "./Section/section.data";
 
 const Footer: FC = () => {
+  const { isMobile } = useMatchMedia();
   return (
     <>
       <div className={style.footer}>
         <div className={style.container_menu}>
-          {sectionData.map((item, i) => (
-            <div key={item.title + i}>
-              <Section item={item.item} title={item.title} />
-            </div>
-          ))}
+          {isMobile
+            ? sectionData.map((item, i) => (
+                <div key={item.title + i}>
+                  <Section item={item.item} title={item.title} />
+                </div>
+              ))
+            : sectionDataMobile.map((item, i) => (
+                <div key={item.title + i}>
+                  <Section item={item.item} title={item.title} />
+                </div>
+              ))}
         </div>
         <ContactsFooter />
       </div>
--- app/Footer/Section/section.data.ts
+++ app/Footer/Section/section.data.ts
@@ -1,4 +1,3 @@
-
 import { ISection } from "./section.interface";
 
 export const sectionData: ISection[] = [
@@ -50,6 +49,72 @@ export const sectionData: ISection[] = [
       {
         title: "Благодарственные письма",
         link: "letters",
+      },
+      {
+        title: "Сертификаты",
+        link: "Certificates",
+      },
+      {
+        title: "Блог на Youtube",
+        link: "Youtube",
+      },
+      {
+        title: "Вопрос / Ответ",
+        link: "Question",
+      },
+    ],
+  },
+];
+
+export const sectionDataMobile: ISection[] = [
+  {
+    title: "о компании",
+    item: [
+      {
+        title: "Партнёрская программа",
+        link: "program",
+      },
+      {
+        title: "Вакансии",
+        link: "Jobs",
+      },
+    ],
+  },
+  {
+    title: "Меню",
+    item: [
+      {
+        title: "Расчёт стоимости",
+        link: "Calculation",
+      },
+      {
+        title: "Услуги",
+        link: "Services",
+      },
+      {
+        title: "Виджеты",
+        link: "Widgets",
+      },
+      {
+        title: "Интеграции",
+        link: "Integrations",
+      },
+      {
+        title: "Наши клиенты",
+        link: "clients",
+      },
+    ],
+  },
+  {
+    title: " ",
+    item: [
+      {
+        title: "Благодарность клиентов",
+        link: "letters",
+      },
+      {
+        title: "Кейсы",
+        link: "Cases",
       },
       {
         title: "Сертификаты",
